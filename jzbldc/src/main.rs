30406fdaaff3bf6ffe9b69ef23c7992d72c7ff6b
epbuennig <inquiries@epbuennig.me>
2023-03-11 19:07:05 +0100

    use descriptive names

--- jzbldc/src/main.rs
+++ jzbldc/src/main.rs
@@ -5,16 +5,18 @@ use classfile::parser;
 type Result<T = (), E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
 fn main() -> Result {
+    // - use descriptive variable names, `file_name` instead of `f`
     // - don't use expect/unwrap, especially if inside a Result-returning function, ok_or returns
     //   the result of the closure if the Option is None, turning it into a Result
-    let f = std::env::args()
+    let file_name = std::env::args()
         .nth(1)
         .ok_or("no filepath given, usage `jzbldc <path>`")?;
 
-    let fs = std::fs::read(f)?;
+    // see above regarding names
+    let file_content = std::fs::read(file_name)?;
 
     // see review of classfile
-    let classfile = parser::parse(&fs)?;
+    let classfile = parser::parse(&file_content)?;
 
     println!("{:?}", classfile.access_flags);
     println!("{:?}", classfile.constant_pool[classfile.super_class + 1]);
